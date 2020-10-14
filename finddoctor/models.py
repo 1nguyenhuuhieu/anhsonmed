ef43556ef1fcf1969d5bdddd883ab99cd0cd1517
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2020-10-14 11:56:36 +0700

    a

--- finddoctor/models.py
+++ finddoctor/models.py
@@ -89,6 +89,7 @@ class Education(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200,null=True, blank=True)
     avatar = models.ImageField(upload_to = 'imgs/avatars/',null=True, blank=True )
     def __str__(self):
         return  str(self.user)
