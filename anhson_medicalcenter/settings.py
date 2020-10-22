63c470615cc9cd378d993a757b98c4868f0656f7
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2020-10-22 11:06:07 +0700

    Update settings.py

--- anhson_medicalcenter/settings.py
+++ anhson_medicalcenter/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'anhson_medicalcenter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
